e0fe281eb60fc993bb7e79f4a7ebe06a1eaa045a
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-05-23 08:43:26 +0200

    today

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 <main> 
   <div class="balance-card"> 
     <p>My balance</p>
+    <h1>$ 1,000.00</h1>
+    <progress value="12.5" max="20"></progress>
   </div>
 </main>
 `;
